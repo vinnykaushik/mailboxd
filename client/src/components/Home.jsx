d64be944f8c6111ef0b9ef4e43694f2ff3e0ab7c
Vinny Kaushik <vin.kaushik23@gmail.com>
2025-03-31 21:32:51 -0400

    Separate packages, add index.jsx

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,31 @@
+import { useAuthUser } from "../security/AuthContext";
+import { useNavigate } from "react-router-dom";
+
+export default function Home() {
+  const { isAuthenticated, user } = useAuthUser();
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <h1>Mailboxd</h1>
+      <div>
+        {!isAuthenticated ? (
+          <div>
+            <p>Welcome to Mailboxd! Log in or register to track your films!</p>
+            <button className="btn-primary" onClick={() => navigate("/login")}>
+              Login
+            </button>
+            <button
+              className="btn-secondary"
+              onClick={() => navigate("/register")}
+            >
+              Create Account
+            </button>
+          </div>
+        ) : (
+          <p>Welcome back, {user?.username}! You are logged in.</p>
+        )}
+      </div>
+    </div>
+  );
+}
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,9 +1,23 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Home from "./components/Home";
 import { AuthProvider } from "./security/AuthContext";
+import * as ReactDomClient from "react-dom/client";
+
+const container = document.getElementById("root");
 
 const root = ReactDomClient.createRoot(container);
 
 root.render(
   <AuthProvider>
-    <BrowserRouter></BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   </AuthProvider>
 );
